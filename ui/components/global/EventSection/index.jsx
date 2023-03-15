e99df221bc4110d3aed0dd7460a520e43cd98df0
Aleksey <aleksey.kosourov@decathlon.com>
2023-03-15 15:46:04 +0300

    add EventItem
    fix EventSection.module.scss
    fix index.jsx

--- ui/components/global/EventSection/index.jsx
+++ ui/components/global/EventSection/index.jsx
@@ -1,36 +1,32 @@
 import React from 'react';
-
-import styles from '@/ui/components/global/EventSection/EventSection.module.scss';
+import { EventItem } from './EventItem';
 
 export default function EventSection() {
   return (
     <div>
-      События
-      <div className={styles.wrapper}>
-        <ul>
-          <li>30</li>
-          <li>Марта</li>
-          <li>2023</li>
-          <li>12:00</li>
-        </ul>
-        <a href='http://'>
-          <img
-            src='https://ic.pics.livejournal.com/dain666/1488818/491213/491213_original.png'
-            alt='done'
-          />
-        </a>
-        <ul>
-          <li>Что: Презентация стримингового сервиса DLESS</li>
-          <li>Где: Google Meet</li>
-          <li>Кто: Представят проект стажёры Декатлона</li>
-        </ul>
-        <div>
-          <button>
-            <a href='http://'> Купить пропуск </a>
-          </button>
-          <li>Ссылка на встречу</li>
-        </div>
-      </div>
+      <h2>События</h2>
+      <EventItem
+        dataEvent={'30'}
+        dataMonth={'Марта'}
+        dataYear={'2023'}
+        dataTime={'15:00'}
+        title={'Презентация стримингового сервиса DLESS'}
+        author={'Проект представят стажёры Декатлона'}
+        img={
+          'https://ic.pics.livejournal.com/dain666/1488818/491213/491213_original.png'
+        }
+      />
+      <EventItem
+        dataEvent={'31'}
+        dataMonth={'Марта'}
+        dataYear={'2023'}
+        dataTime={'12:00'}
+        title={'Декатлон: франшиза или закрытие?'}
+        author={'Жереми Анжелар'}
+        img={
+          'https://play-lh.googleusercontent.com/5SA-3vo78uGvVrTQ-Or-XrjYMum8r7lAgmGxPHP5aWUidCBUuJbZbny8M2c8NWNJPRo'
+        }
+      />
     </div>
   );
 }
