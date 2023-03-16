8246b6ed92b158640808f258f01f057399852417
Samokhin Daniil <daniil.samokhin@decathlon.com>
2023-03-16 14:35:16 +0300

    Fix playlist

--- ui/components/UsersPlaylist/UserPlaylist_item/index.jsx
+++ ui/components/UsersPlaylist/UserPlaylist_item/index.jsx
@@ -1,47 +1,12 @@
 import React, { useState } from 'react';
-import { useRouter } from 'next/router';
-import styles from './UsersPlaylist_item.module.scss';
-import Image from 'next/image';
+import styles from '@/ui/components/UsersPlaylist/UserPlaylist_item/UsersPlaylist_item.module.scss';
+import Playlist_item from './index_item';
 
 const UserPlaylist_item = ({ audio }) => {
-  const [isPlay, setIsPlay] = useState(true);
-  const router = useRouter();
-
   return (
     <div className={styles.wrapper}>
       {audio.map((audio, index) => (
-        <div className={styles.playlist_item} key={index}>
-          <div className={styles.header}>
-            <Image
-              className={styles.image}
-              src={audio.image}
-              width={150}
-              height={150}
-              alt={audio.nameplaylist}
-              onClick={() => {
-                router.push(`/playlist/${audio.id}`);
-              }}
-            />
-          </div>
-          <div className={styles.footer}>
-            <div className={styles.subtitle}>
-              <span className={styles.author}>Author: {audio.author}</span>
-              <p className={styles.nameplaylist}>
-                Playlist: {audio.nameplaylist}
-              </p>
-            </div>
-            <div className={styles.options}>
-              <div
-                className={`${styles.playerBox__play} ${
-                  !isPlay ? styles.active : ''
-                }`}
-                onClick={() => {
-                  setIsPlay(!isPlay);
-                }}></div>
-              <i className='fa-regular fa-heart'></i>
-            </div>
-          </div>
-        </div>
+        <Playlist_item audio={audio} key={index} />
       ))}
     </div>
   );
