e4b662a5e867140f819afc8c40682d00261eb3c0
Samokhin Daniil <daniil.samokhin@decathlon.com>
2023-03-16 16:53:34 +0300

    design

--- pages/playlist/[id].jsx
+++ pages/playlist/[id].jsx
@@ -1,7 +1,20 @@
 import React from 'react';
+import Head from 'next/head';
+import Script from 'next/script';
+import Playlist_Page from '@/ui/components/Playlist_Page';
 
 const Details = () => {
-  return <div>[id]</div>;
+  return (
+    <>
+      <Head>
+        <meta name='description' content='Decathlon lessons project' />
+        <meta name='viewport' content='width=device-width, initial-scale=1' />
+        <link rel='icon' href='/favicon.ico' />
+      </Head>
+      <Script src='https://kit.fontawesome.com/fb72704844.js' />
+      <Playlist_Page />
+    </>
+  );
 };
 
 export default Details;
---
+++ ui/components/Playlist_Page/index.jsx
@@ -0,0 +1,56 @@
+import React, { useState } from 'react';
+import Image from 'next/image';
+
+import styles from '@/ui/components/Playlist_Page/Playlist_Page.module.scss';
+const Playlist_Page = () => {
+  const [isPlay, setIsPlay] = useState(true);
+
+  return (
+    <div className='container'>
+      <div className={styles.wrapper}>
+        <div className={styles.left_side}>
+          <div className='image'>
+            <Image
+              src={'https://via.placeholder.com/300x300'}
+              width={300}
+              height={300}
+              alt={'Image playlist'}
+            />
+          </div>
+          <div className={styles.description}>
+            <div className={styles.release}>
+              Release: <span className={styles.date}>2023-03-30</span>
+            </div>
+            <div className={styles.rating}>
+              Rating: <span className={styles.number}>******</span>
+            </div>
+          </div>
+        </div>
+        <div className={styles.rigth_side}>
+          <div>Likes</div>
+          <div>Playlst name</div>
+          <div>Author</div>
+          <div className={styles.list_item}>
+            <div className={styles.number}>1</div>
+            <div className={styles.list_item_music}>
+              <div className={styles.author}>Автор песенки</div>
+              <div>название песенки</div>
+            </div>
+            <div className={styles.options}>
+              <div
+                className={`${styles.playerBox__play} ${
+                  !isPlay ? styles.active : ''
+                }`}
+                onClick={() => {
+                  setIsPlay(!isPlay);
+                }}></div>
+              <i className='fa-regular fa-heart'></i>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Playlist_Page;
