65eb4295bce298d4c7c3d319839b86ab2c79a26f
Aleksey <aleksey.kosourov@decathlon.com>
2023-03-19 15:52:16 +0300

    ui fix

--- pages/profile.jsx
+++ pages/profile.jsx
@@ -4,6 +4,7 @@ import { getServerSession } from 'next-auth/next';
 import { useRouter } from 'next/router';
 import { useState } from 'react';
 import Layout from '@/ui/components/Sidebar/Layout';
+import Image from 'next/image';
 
 import styles from '../ui/components/Sidebar/Layout/Profile.module.scss';
 
@@ -46,26 +47,28 @@ const profile = ({ sessionServer }) => {
   return (
     <>
       <Layout>
-        <div>
-          <ul className={styles.userStat__list}>
-            <li className={styles.userStat__item}>
-              <p className={styles.userStat__title}>Liked</p>
-              <span>33</span>
-            </li>
-            <li className={styles.userStat__item}>
-              <p className={styles.userStat__title}>Listened</p>
-              <span>476 ч</span>
-            </li>
-            <li className={styles.userStat__item}>
-              <p className={styles.userStat__title}>Created playlists</p>
-              <span>8</span>
-            </li>
-          </ul>
-        </div>
+        <h3 className={styles.title}>Statistics</h3>
+        <ul className={styles.userStat__list}>
+          <li className={styles.userStat__item}>
+            <p className={styles.userStat__title}>Liked</p>
+            <span>33</span>
+          </li>
+          <li className={styles.userStat__item}>
+            <p className={styles.userStat__title}>Listened</p>
+            <span>476 ч</span>
+          </li>
+          <li className={styles.userStat__item}>
+            <p className={styles.userStat__title}>Playlists</p>
+            <span>8</span>
+          </li>
+        </ul>
+        <Image width={120} height={120} />
         <ul className={styles.userInfo}>
-          <li>Name: {user.name ? user.name : 'Не указано'}</li>
-          <li>Username: {user.username}</li>
-          <li>Country: Россия</li>
+          <li className={styles.userInfo__item}>
+            Name: {user.name ? user.name : 'Не указано'}
+          </li>
+          <li className={styles.userInfo__item}>Username: {user.username}</li>
+          <li className={styles.userInfo__item}>Email: {user.email}</li>
         </ul>
       </Layout>
       <button onClick={handle}>Log out</button>
