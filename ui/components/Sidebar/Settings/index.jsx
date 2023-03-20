7a610fb8871c429f012be7b36246145c12718bfa
Aleksey <aleksey.kosourov@decathlon.com>
2023-03-20 23:24:09 +0300

    add switch

--- ui/components/Sidebar/Settings/index.jsx
+++ ui/components/Sidebar/Settings/index.jsx
@@ -1,30 +1,25 @@
-import React, { useState } from 'react';
+import React, from 'react';
+import Switch from '../Switch';
 
 import styles from '../Settings/Settings.module.scss';
-import Input from '../../Form/Input';
 
-export default function Settings({ userName, userEmail, userPassword }) {
-  const [name, setName] = useState(userName);
-  const [email, setEmail] = useState(userEmail);
-  const [password, setPassword] = useState(userPassword);
-
-  const handleChangeValue = (event) => {
-    setName(event.target.value);
-  };
-
-  const handleChangeEmail = (event) => {
-    setEmail(event.target.value);
-  };
-  const handleChangePassword = (event) => {
-    setPassword(event.target.value);
-  };
-
+export default function Settings() {
   return (
     <>
       <h3 className={styles.title}>
         <span>S</span>ettings
       </h3>
-      <div className={styles.wrapper}></div>
+      <div className={styles.wrapper}>
+        <ul className={styles.settings__list}>
+          <li className={styles.settings__item}>Воспроизводить бесконечно <Switch /></li>
+          <li className={styles.settings__item}>
+            Воспроизводить автоматически <Switch />
+          </li>
+          <li className={styles.settings__item}>Погладить кота <Switch /></li>
+          <li className={styles.settings__item}>Не гладить кота <Switch /></li>
+          <li className={styles.settings__item}>Что за настройка? <Switch /></li>
+        </ul>
+      </div>
     </>
   );
 }
