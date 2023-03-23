2f737c89b5b03f6814909a6fa934f60838b01da0
Samokhin Daniil <daniil.samokhin@decathlon.com>
2023-03-23 10:11:07 +0300

    fix

--- ui/components/Top10/index.jsx
+++ ui/components/Top10/index.jsx
@@ -1,16 +1,16 @@
-import React from 'react';
-import styles from '@/ui/components/Top10/Top10.module.scss';
-import Track from '../Track';
+import React from 'react'
+import styles from '@/ui/components/Top10/Top10.module.scss'
+import Track from '../Track'
 
 const Top10 = ({ audioTop }) => {
   return (
-    <div className={styles.top10}>
+    <div className={styles.wrapper}>
       <div className='title'>Топ 10</div>
       {audioTop.data?.map(({ id, attributes }, index) => (
         <Track key={id} id={id} index={index + 1} attributes={attributes} />
       ))}
     </div>
-  );
-};
+  )
+}
 
-export default Top10;
+export default Top10
