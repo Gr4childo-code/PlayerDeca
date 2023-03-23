6b4e80b568486c4da2f17f4a628052339c80f24e
Aleksey <aleksey.kosourov@decathlon.com>
2023-03-23 13:33:57 +0300

    fix sizes placeholder

--- ui/components/global/Slider/dataImage3.js
+++ ui/components/global/Slider/dataImage3.js
@@ -1,9 +1,9 @@
 const images3 = [
-	'https://via.placeholder.com/400x200/901C3F/FFFFFF',
-	'https://via.placeholder.com/400x200/FF3783/FFFFFF',
-	'https://via.placeholder.com/400x200/C70039/FFFFFF',
-	'https://via.placeholder.com/400x200/900C3F/FFFFFF',
-	'https://via.placeholder.com/400x200/FF5743/FFFFFF',
+	'https://via.placeholder.com/946x500/901C3F/FFFFFF',
+	'https://via.placeholder.com/946x500/FF3783/FFFFFF',
+	'https://via.placeholder.com/946x500/C70039/FFFFFF',
+	'https://via.placeholder.com/946x500/900C3F/FFFFFF',
+	'https://via.placeholder.com/946x500/FF5743/FFFFFF',
 ];
 
 export default images3
