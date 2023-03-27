b59ec013a23c326ed7913b9aadba59addb39ab98
Aleksey <aleksey.kosourov@decathlon.com>
2023-03-27 11:04:04 +0300

    refactor routing profile

--- pages/profile/my-playlists/index.jsx
+++ pages/profile/my-playlists/index.jsx
@@ -1,11 +1,11 @@
 import React from 'react';
 import Layout from '@/ui/components/Sidebar/Layout';
 import Playlists from '@/ui/components/Sidebar/Playlists';
-import img1 from '../ui/components/Sidebar/Playlists/img/img1.jpeg';
-import img2 from '../ui/components/Sidebar/Playlists/img/img2.webp';
-import img3 from '../ui/components/Sidebar/Playlists/img/img3.jpg';
-import img4 from '../ui/components/Sidebar/Playlists/img/img4.jpeg';
-import img5 from '../ui/components/Sidebar/Playlists/img/img5.png';
+import img1 from '../../../ui/components/Sidebar/Playlists/img/img1.jpeg';
+import img2 from '../../../ui/components/Sidebar/Playlists/img/img2.webp';
+import img3 from '../../../ui/components/Sidebar/Playlists/img/img3.jpg';
+import img4 from '../../../ui/components/Sidebar/Playlists/img/img4.jpeg';
+import img5 from '../../../ui/components/Sidebar/Playlists/img/img5.png';
 
 export default function MyPlaylists() {
   const playlistsData = [
--- ui/components/Sidebar/index.jsx
+++ ui/components/Sidebar/index.jsx
@@ -6,10 +6,10 @@ import styles from './Sidebar.module.scss';
 export default function Sidebar() {
   const menuItems = [
     { id: 1, label: 'Main', link: '/profile' },
-    { id: 2, label: 'Account', link: '/account' },
-    { id: 3, label: 'My playlists', link: '/my-playlists' },
-    { id: 4, label: 'Upload', link: '/upload' },
-    { id: 5, label: 'Settings', link: '/settings' },
+    { id: 2, label: 'Account', link: '/profile/account' },
+    { id: 3, label: 'My playlists', link: '/profile/my-playlists' },
+    { id: 4, label: 'Upload', link: '/profile/upload' },
+    { id: 5, label: 'Settings', link: '/profile/settings' },
   ];
 
   return (
@@ -18,7 +18,7 @@ export default function Sidebar() {
         {menuItems.map(({ id, label, link }) => {
           return (
             <Link key={id} href={link}>
-              {label} {/* Нужно ли здесь li ? */}
+              {label}
             </Link>
           );
         })}
