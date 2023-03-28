5a466e49fe6e66b783139d99a48cea477708618d
Aleksey <aleksey.kosourov@decathlon.com>
2023-03-28 19:46:13 +0300

    fix

--- ui/components/global/Slider/SlideDescription/index.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-import styles from '@/ui/components/global/Slider/SlideDescription/SlideDescription.module.scss';
-
-export default function SlideDescription({ filter, description }) {
-  console.log(description);
-
-  return <></>;
-}
