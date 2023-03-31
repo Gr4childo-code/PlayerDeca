802c35ab166014d357758b56e20188ab2eec70a5
Aleksandr <salikweb@yandex.ru>
2023-04-01 01:03:51 +0300

    edit 404 page

--- ui/components/global/404/index.jsx
+++ ui/components/global/404/index.jsx
@@ -1,27 +1,14 @@
 import React from 'react';
+import Image from 'next/image'
 
 import styles from '@/ui/components/global/404/404.module.scss';
 
 function NotFound(props) {
   return (
-    <div>
-      <div className={styles.notFoundWrapper}>
-        <h1>404 - Страница не найдена</h1>
-        <p>Страница не существует или произошла ошибка</p>
-        <ul className={styles.linkList}>
-          <li className={styles.linkList_item}>
-            <a href='http://'>Главная</a>
-          </li>
-          <li className={styles.linkList_item}>
-            <a href='http://'>Музыка</a>
-          </li>
-          <li className={styles.linkList_item}>
-            <a href='http://'>Личный кабинет</a>
-          </li>
-        </ul>
-      </div>
+    <div className={styles.notFound}>
+      <Image src='/404.svg' width='600' height="600" />
     </div>
-  );
+  )
 }
 
 export default NotFound;
