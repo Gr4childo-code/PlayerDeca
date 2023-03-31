0c3450edecdf56cc3edbc37d198aea321ad2e500
Gr4childo <samohkin.2012@gmail.com>
2023-03-31 15:58:21 +0300

    add icon

--- pages/profile/index.jsx
+++ pages/profile/index.jsx
@@ -1,13 +1,16 @@
 import { signOut, getSession } from 'next-auth/react';
 import { useRouter } from 'next/router';
+import Image from 'next/image';
 import Layout from '@/ui/components/Sidebar/Layout';
 
 import styles from '../../ui/components/Sidebar/Layout/Profile.module.scss';
 
 const profile = ({ session }) => {
   const router = useRouter();
-  const user = session.user
-
+  const user = session.user;
+  const customLoader = () => {
+    return `https://api.dless.ru${user?.image}`;
+  };
   const signOutHandle = async () => {
     const { url } = await signOut({
       redirect: false,
@@ -16,17 +19,28 @@ const profile = ({ session }) => {
 
     router.push(url);
   };
-
+  console.log(user);
   return (
     <Layout>
       <div className={styles.wrapper}>
         <ul className={styles.userInfo}>
           <li className={styles.userInfo__item}>
-            <p className={styles.userInfo__title}>Name:</p>
-            <p className={styles.userInfo__descr}>
-              {user?.name ? user.name : 'Не указано'}
-            </p>
+            {user?.image && (
+              <Image
+                src='profilePic.webp'
+                width={300}
+                height={300}
+                alt='User profile picture'
+                quality={10}
+                loader={customLoader}
+              />
+            )}
           </li>
+          <li className={styles.userInfo__item}>
+            <p className={styles.userInfo__title}>Name:</p>
+            <p className={styles.userInfo__descr}></p>
+          </li>
+
           <li className={styles.userInfo__item}>
             <p className={styles.userInfo__title}>Username:</p>
             <p className={styles.userInfo__descr}>{user?.username}</p>
@@ -49,7 +63,7 @@ export default profile;
 export const getServerSideProps = async (ctx) => {
   return {
     props: {
-      session: await getSession(ctx)
-    }
-  }
-}
+      session: await getSession(ctx),
+    },
+  };
+};
--- ui/components/global/AuthBadge/index.jsx
+++ ui/components/global/AuthBadge/index.jsx
@@ -1,33 +1,42 @@
-import Link from 'next/link'
-import Image from 'next/image'
-import { useSession } from "next-auth/react"
+import Link from 'next/link';
+import Image from 'next/image';
+import { useSession } from 'next-auth/react';
 
-import styles from './AuthBadge.module.scss'
+import styles from './AuthBadge.module.scss';
 
 const AuthBadge = () => {
-  const { data: session } = useSession()
+  const { data: session } = useSession();
 
   const customLoader = () => {
-    return `https://api.dless.ru${session?.user?.image}`
-  }
+    return `https://api.dless.ru${session?.user?.image}`;
+  };
 
   return (
-    <Link href={session ? '/profile' : '/auth/login'} className={styles.authBadge}>
+    <Link
+      href={session ? '/profile' : '/auth/login'}
+      className={styles.authBadge}
+    >
       <div className={styles.authBadge__name}>
-        {session ? session?.user?.name ? session?.user?.name : session?.user?.username : 'Авторизация' }
+        {session
+          ? session?.user?.name
+            ? session?.user?.name
+            : session?.user?.username
+          : 'Авторизация'}
       </div>
       <div className={styles.authBadge__ava}>
-        {session?.user?.image && <Image
-          src="profilePic.webp"
-          width={34}
-          height={34}
-          alt="User profile picture"
-          quality={80}
-          loader={customLoader}
-        />}
+        {session?.user?.image && (
+          <Image
+            src='profilePic.webp'
+            width={34}
+            height={34}
+            alt='User profile picture'
+            quality={80}
+            loader={customLoader}
+          />
+        )}
       </div>
     </Link>
-  )
-}
+  );
+};
 
-export default AuthBadge
+export default AuthBadge;
