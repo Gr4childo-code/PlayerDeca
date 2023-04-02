aa0474da65a5bcdf5de6f3472f9913c9c3f343a6
Aleksandr <salikweb@yandex.ru>
2023-04-02 21:35:21 +0300

    refactor index

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,14 +1,11 @@
 //Next/React
 import Head from 'next/head';
-import { useState } from 'react';
-import Link from 'next/link';
 
 //Components
 import SliderEvents from '@/ui/components/global/Slider/SliderEvents';
 import SliderPlaylists from '@/ui/components/global/Slider/SliderPlaylists';
 
 import Top10 from '@/ui/components/Top10';
-import Toast from '@/ui/components/global/Toast';
 import EventsAll from '@/ui/components/DlessEvents/EventsAll';
 
 //Utils
@@ -30,19 +27,6 @@ export const getServerSideProps = async () => {
 };
 
 export default function Home({ audioTop, playlists, events }) {
-  const [list, setList] = useState([]);
-  let toastItem = null;
-
-  const showToast = ({ type, title, description }) => {
-    toastItem = {
-      id: list.length + 1,
-      type,
-      title,
-      description,
-    };
-    setList([...list, toastItem]);
-  };
-
   return (
     <>
       <Head>
@@ -50,43 +34,13 @@ export default function Home({ audioTop, playlists, events }) {
         <meta name='viewport' content='width=device-width, initial-scale=1' />
         <link rel='icon' href='/favicon.ico' />
       </Head>
-      <Toast toastlist={list} setList={setList} />
-      {/* <button
-        onClick={() => {
-          showToast({
-            type: 'error',
-            title: 'Error',
-            description: 'API has been deleted',
-          });
-        }}>
-        Error
-      </button> */}
-      {/* <button
-        onClick={() => {
-          showToast({
-            type: 'warn',
-            title: 'Warn',
-            description: 'Type password',
-          });
-        }}>
-        Warn
-      </button>
-      <button
-        onClick={() => {
-          showToast({
-            type: 'success',
-            title: 'Success',
-            description: 'Complete',
-          });
-        }}>
-        Success
-      </button> */}
+
       <div className='container'>
         <div className='layout'>
           <div className='layout__left'>
             <div className='title'>События DLESS</div>
             <SliderEvents events={events} buttons={true} pagination={true} />
-            <div class='title'>Новинки от пользователей</div>
+            <div className='title'>Новинки от пользователей</div>
             <SliderPlaylists
               playlists={playlists}
               buttons={false}
