293e1b1a50db5fc882c8883bdd3054a231dbc39e
Aleksey <aleksey.kosourov@decathlon.com>
2023-04-03 13:19:13 +0300

    ref fetch, add old password

--- pages/profile/settings/index.jsx
+++ pages/profile/settings/index.jsx
@@ -2,7 +2,6 @@ import React, { useState } from 'react';
 import { getSession } from 'next-auth/react';
 import Layout from '@/ui/components/Sidebar/Layout';
 import styles from '../../../ui/components/Sidebar/ProfileSettings/Settings.module.scss';
-import { fetchAPI } from '@/utils/api/fetch';
 
 export const getServerSideProps = async (ctx) => {
   const session = await getSession(ctx);
@@ -23,17 +22,26 @@ export default function ProfileSettings({ user }) {
   const [currentUser, setNewUser] = useState(user);
   const [newName, setNewName] = useState('');
   const [newEmail, setNewEmail] = useState('');
-  const [сurrentPassword, setCurrentPassword] = useState('');
+  const [oldPassword, setOldPassword] = useState('');
   const [password, setPassword] = useState('');
   const [passwordConfirmation, setPasswordConfirmation] = useState('');
   console.log(currentUser);
 
-  const updatePassword = async () => {
-    const response = await fetchAPI(`/auth/change-password`, 'POST', {
-      currentPassword: сurrentPassword,
-      password: password,
-      passwordConfirmation: password,
-    });
+  const updatePassword = async (ctx) => {
+    const session = await getSession(ctx);
+
+    const { oldPassword, newPassword, confirmPassword } = session.user;
+    const response = await fetch(
+      `${process.env.NEXT_PUBLIC_API_URL}/auth/change-password`,
+      {
+        method: 'PUT',
+        headers: {
+          Authorization: `Bearer ${process.env.NEXT_PUBLIC_API_TOKEN}`,
+          'Content-type': 'application/json',
+        },
+        body: JSON.stringify({ oldPassword, newPassword }),
+      }
+    );
     const data = response.json();
     console.log(data);
   };
@@ -46,7 +54,7 @@ export default function ProfileSettings({ user }) {
       <div className={styles.wrapper}>
         <div className={styles.user}>
           <p>id пользователя : {id}</p>
-          <p>Имя пользователя : {name}</p>
+          <p>Имя пользователя : {name || 'Нет имени'}</p>
           <p>Емайл пользователя : {email}</p>
         </div>
         <ul className={styles.settings__list}>
@@ -82,9 +90,9 @@ export default function ProfileSettings({ user }) {
             <input
               className={styles.settings__input}
               type={'password'}
-              value={сurrentPassword}
-              placeholder='Введите текущий пароль'
-              onChange={(e) => setCurrentPassword(e.target.value)}
+              value={oldPassword}
+              placeholder='Старый пароль'
+              onChange={(e) => setOldPassword(e.target.value)}
             />
           </li>
           <li className={styles.settings__item}>
@@ -92,7 +100,7 @@ export default function ProfileSettings({ user }) {
               className={styles.settings__input}
               type={'password'}
               value={password}
-              placeholder='Введите новый пароль'
+              placeholder='Новый пароль'
               onChange={(e) => setPassword(e.target.value)}
             />
           </li>
