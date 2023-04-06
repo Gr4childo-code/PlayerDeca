eb50d8d49ce2105d07a3b76aa7db62efc5cbd5fb
Aleksey <aleksey.kosourov@decathlon.com>
2023-04-06 12:33:07 +0300

    fix

--- ui/components/Sidebar/ProfileUpload/index.jsx
+++ ui/components/Sidebar/ProfileUpload/index.jsx
@@ -1,16 +1,61 @@
-import React from 'react';
+import React, { useRef, useState } from 'react';
+import { useSession } from 'next-auth/react';
+
+import { createAudios } from '@/api';
 
 import styles from '../ProfileUpload/Dropzone.module.scss';
 
 export default function ProfileUpload() {
+  const music = useRef(null);
+  const poster = useRef(null);
+  const [loader, setLoader] = useState(true);
+  const [title, setTitle] = useState('');
+  const [author, setAuthor] = useState('');
+  const { data: session } = useSession();
+
+  const uploads = async (e) => {
+    e.preventDefault();
+
+    if (session) {
+      setLoader(false);
+
+      createAudios(
+        {
+          data: {
+            name: '',
+            author: '',
+          },
+          files: {
+            src: music.current,
+            poster: poster.current,
+          },
+        },
+        session?.jwt
+      ).then(() => setLoader(true));
+    }
+  };
+
   return (
     <>
-      <div className={styles.wrapper}>
-        <input type={'file'} />
-      </div>
-      <div>Загруженная песня</div>
-      <div>Загруженная песня</div>
-      <div>Загруженная песня</div>
+      {loader ? (
+        <form onSubmit={uploads}>
+          <div className={styles.wrapper}>
+            <input
+              className={styles.input}
+              type={'file'}
+              onChange={(e) => (music.current = e.target.files[0])}
+            />
+            <input
+              className={styles.input}
+              type={'file'}
+              onChange={(e) => (poster.current = e.target.files[0])}
+            />
+            <button className={styles.button}>upload</button>
+          </div>
+        </form>
+      ) : (
+        <div>Loading...</div>
+      )}
     </>
   );
 }
