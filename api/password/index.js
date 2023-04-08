75440ff1870d3dbe8ade92bd632eb6d7dfde2f82
Aleksey <aleksey.kosourov@decathlon.com>
2023-04-08 15:35:08 +0300

    fix

--- api/password/index.js
+++ api/password/index.js
@@ -1,7 +1,4 @@
 import { Api } from '@/api'
 import { stringify } from 'qs'
 
-export const postPassword = async (params, token) => {
-  Api('auth/change-password', 'PUT', params, token)
-
-}
+export const postPassword = async (params, token) => Api('auth/change-password', 'PUT', params, token)
