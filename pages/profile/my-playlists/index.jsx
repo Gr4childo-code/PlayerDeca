881267146e1dc78578bb5565cac8337914763b3f
Aleksey <aleksey.kosourov@decathlon.com>
2023-04-12 17:43:48 +0300

    fix collection

--- pages/profile/my-playlists/index.jsx
+++ pages/profile/my-playlists/index.jsx
@@ -1,24 +1,8 @@
 import React from 'react';
 import Layout from '@/ui/components/Sidebar/Layout';
 import ProfileUpload from '@/ui/components/Sidebar/ProfileUpload';
-import { getSession } from 'next-auth/react';
-import { getAudios, getAudiosForUser } from '@/api';
 
-export const getServerSideProps = async (ctx) => {
-  const session = await getSession(ctx);
-  const userId = session.user.id;
-  const username = session.user.username;
-  const token = session.jwt;
-
-  const test = await getAudios();
-
-  return {
-    props: { userId, token, username, test },
-  };
-};
-
-export default function MyPlaylists({ username, test }) {
-  console.log(username, test);
+export default function MyPlaylists() {
   return (
     <Layout>
       <div className='collectionPlaylists'>
