3160140315980c31f38536e48a98a46b185adb99
Gr4childo <samohkin.2012@gmail.com>
2023-04-14 10:10:54 +0300

    add setAudioContext

--- ui/components/AllAudios_Page/index.jsx
+++ ui/components/AllAudios_Page/index.jsx
@@ -1,9 +1,15 @@
-import { useState } from 'react';
+import { useState, useContext } from 'react';
+import AppContext from '@/ui/components/global/AppContext';
+
+import Options from '@/ui/components/Options';
 import Track from '@/ui/components/Track';
 import styles from '@/ui/components/AllAudios_Page/AllAudios_Page.module.scss';
-import Options from '@/ui/components/Options';
+
 const AllAudios_Page = ({ audios }) => {
+  const { setAudioContext } = useContext(AppContext);
+
   const [visible, setVisibale] = useState(10);
+
   const showMoreTrack = () => {
     setVisibale((prevValue) => prevValue + 5);
   };
@@ -11,7 +17,7 @@ const AllAudios_Page = ({ audios }) => {
   return (
     <div className={styles.wrapper}>
       <div className={'title'}>Все треки</div>
-      <div className={styles.options}>
+      <div className={styles.options} onClick={() => setAudioContext(audios)}>
         <Options play={true} queue={true} size={'lg'} />
       </div>
       {audios ? (
