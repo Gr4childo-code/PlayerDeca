a0360f39847f971238f65dbcad944e447b9f67d3
Aleksey <aleksey.kosourov@decathlon.com>
2023-04-15 13:05:44 +0300

    refactor dragndrop

--- pages/profile/my-playlists/index.jsx
+++ pages/profile/my-playlists/index.jsx
@@ -1,14 +1,98 @@
-import React from 'react';
+import React, { useState, useRef } from 'react';
+import { useSession } from 'next-auth/react';
 import Layout from '@/ui/components/Sidebar/Layout';
 import DragAndDrop from '@/ui/components/Sidebar/ProfileUpload';
 
+import { createAudios } from '@/api';
+
 export default function UserCollection() {
+  const [playlist, setPlaylist] = useState([]);
+  const poster = useRef(null);
+  const [loader, setLoader] = useState(true);
+  const [file, setFile] = useState([] || null);
+  const { data: session } = useSession();
+
+  const handleDrag = (e) => {
+    e.preventDefault();
+    e.stopPropagation();
+
+    if (e.type === 'dragenter' || e.type === 'dragover') {
+      setLoader(true);
+    } else if (e.type === 'dragleave') {
+      setLoader(false);
+    }
+  };
+
+  const handleSelectFile = (e) => {
+    e.preventDefault();
+    e.stopPropagation();
+    /* setLoader(false); */
+
+    if (e.dataTransfer) {
+      const audioData = e.dataTransfer.files[0].name.split('.mp3', [1]);
+      const audio = {
+        name: audioData[0].split(' - ')[1],
+        author: audioData[0].split(' - ')[0],
+        src: e.dataTransfer.files[0],
+      };
+      /* createAudios(
+        {
+          data: {
+            name: audio.name,
+            author: audio.author,
+          },
+          files: {
+            src: audio.src,
+            poster: poster.current,
+          },
+        },
+        session?.jwt
+      )
+        .then(() => setLoader(true))
+        .then(() => setFile([...file, audio])); */
+      setFile([...file, audio]); // потом удалить
+      console.log(file);
+    } else if (e.target.files) {
+      const audioData = e.target.files[0].name.split('.mp3', [1]);
+      const audio = {
+        name: audioData[0].split(' - ')[1],
+        author: audioData[0].split(' - ')[0],
+        src: e.target.files[0],
+      };
+      /* createAudios(
+        {
+          data: {
+            name: audio.name,
+            author: audio.author,
+          },
+          files: {
+            src: e.target.files[0],
+            poster: poster.current,
+          },
+        },
+        session?.jwt
+      )
+        .then(() => setLoader(true))
+        .then(() => setFile([...file, audio])); */
+      setFile([...file, audio]); //потом удалить
+    }
+  };
+
   return (
     <Layout>
       <div>
-        <DragAndDrop />
+        <DragAndDrop
+          file={file}
+          loader={loader}
+          poster={poster}
+          handleDrag={handleDrag}
+          handleSelectFile={handleSelectFile}
+        />
       </div>
-      <div>{/* Сюда нужно плейлисты пользователя */}</div>
+      <div>
+        Создать плейлист
+        <p>{playlist}</p>
+      </div>
     </Layout>
   );
 }
--- ui/components/Sidebar/ProfileUpload/index.jsx
+++ ui/components/Sidebar/ProfileUpload/index.jsx
@@ -1,83 +1,15 @@
 import React, { useRef, useState, useEffect, use } from 'react';
-import { useSession } from 'next-auth/react';
 import Preloader from '@/ui/components/global/Preloader';
-
-import { createAudios } from '@/api';
 
 import styles from '@/ui/components/Sidebar/ProfileUpload/Dropzone.module.scss';
 
-export default function DragAndDrop() {
-  const poster = useRef(null);
-  const [loader, setLoader] = useState(true);
-  const [file, setFile] = useState([]);
-  const { data: session } = useSession();
-  const [playlist, setPlaylist] = useState([]);
-
-  const handleDrag = (e) => {
-    e.preventDefault();
-    e.stopPropagation();
-
-    if (e.type === 'dragenter' || e.type === 'dragover') {
-      setLoader(true);
-    } else if (e.type === 'dragleave') {
-      setLoader(false);
-    }
-  };
-
-  const handleSelectFile = (e) => {
-    e.preventDefault();
-    e.stopPropagation();
-    /* setLoader(false); */
-
-    if (e.dataTransfer) {
-      const audioData = e.dataTransfer.files[0].name.split('.mp3', [1]);
-      const audio = {
-        name: audioData[0].split(' - ')[1],
-        author: audioData[0].split(' - ')[0],
-        src: e.dataTransfer.files[0],
-      };
-      /* createAudios(
-        {
-          data: {
-            name: audio.name,
-            author: audio.author,
-          },
-          files: {
-            src: audio.src,
-            poster: poster.current,
-          },
-        },
-        session?.jwt
-      )
-        .then(() => setLoader(true))
-        .then(() => setFile([...file, audio])); */
-      setFile([...file, audio]); // потом удалить
-      console.log(file);
-    } else if (e.target.files) {
-      const audioData = e.target.files[0].name.split('.mp3', [1]);
-      const audio = {
-        name: audioData[0].split(' - ')[1],
-        author: audioData[0].split(' - ')[0],
-        src: e.target.files[0],
-      };
-      /* createAudios(
-        {
-          data: {
-            name: audio.name,
-            author: audio.author,
-          },
-          files: {
-            src: e.target.files[0],
-            poster: poster.current,
-          },
-        },
-        session?.jwt
-      )
-        .then(() => setLoader(true))
-        .then(() => setFile([...file, audio])); */
-      setFile([...file, audio]); //потом удалить
-    }
-  };
+export default function DragAndDrop({
+  handleDrag,
+  handleSelectFile,
+  loader,
+  poster,
+  file,
+}) {
   return (
     <div>
       <div className={styles.wrapper}>
@@ -140,7 +72,6 @@ export default function DragAndDrop() {
           <Preloader />
         )}
       </div>
-      <div>Создать плейлист: {playlist}</div>
     </div>
   );
 }
