ccb2168e7b714f262fd5f37ea76e50c2092fa1f7
Aleksey <aleksey.kosourov@decathlon.com>
2023-04-16 23:03:26 +0300

    add notify, delete anything

--- pages/profile/my-playlists/index.jsx
+++ pages/profile/my-playlists/index.jsx
@@ -4,31 +4,33 @@ import Layout from '@/ui/components/Sidebar/Layout';
 import DragAndDrop from '@/ui/components/Sidebar/ProfileUpload';
 import CreatePlaylist from '@/ui/components/Sidebar/ProfileUpload/CreatePlaylist';
 
+import Toast from '@/ui/components/global/Toast';
+
 import { createAudios, postPlaylist } from '@/api';
 
 export default function UserCollection() {
-  const [playlist, setPlaylist] = useState([]);
-  const [playlistName, setPlaylistName] = useState('');
   const poster = useRef(null);
-  const [loader, setLoader] = useState(true);
-  const [files, setFiles] = useState([] || '');
+  const [list, setList] = useState([]);
   const { data: session } = useSession();
+  const [loader, setLoader] = useState(true);
+  const [playlist, setPlaylist] = useState([]);
+  const [files, setFiles] = useState([] || '');
+  const [playlistName, setPlaylistName] = useState('');
 
   const handleDrag = (e) => {
     e.preventDefault();
     e.stopPropagation();
 
     if (e.type === 'dragenter' || e.type === 'dragover') {
-      /* setLoader(true); */
+      setLoader(true);
     } else if (e.type === 'dragleave') {
-      /* setLoader(false); */
+      setLoader(false);
     }
   };
 
   const handleSelectFile = (e) => {
     e.preventDefault();
     e.stopPropagation();
-    /* setLoader(false); */
 
     if (e.dataTransfer) {
       const audioData = e.dataTransfer.files[0].name.split('.mp3', [1]);
@@ -38,24 +40,6 @@ export default function UserCollection() {
         src: e.dataTransfer.files[0],
       };
       setFiles([...files, audio]);
-      console.log(files);
-      /* createAudios(
-        {
-          data: {
-            name: audio.name,
-            author: audio.author,
-          },
-          files: {
-            src: audio.src,
-            poster: poster.current,
-          },
-        },
-        session?.jwt
-      )
-        .then(() => setLoader(true))
-        .then(() => setFiles([...files, audio])); */
-
-      /* setFiles([...files, audio]); */ // потом удалить
     } else if (e.target.files) {
       const audioData = e.target.files[0].name.split('.mp3', [1]);
       const audio = {
@@ -64,36 +48,15 @@ export default function UserCollection() {
         src: e.target.files[0],
       };
       setFiles([...files, audio]);
-      console.log(files);
-      /* createAudios(
-        {
-          data: {
-            name: audio.name,
-            author: audio.author,
-          },
-          files: {
-            src: e.target.files[0],
-            poster: poster.current,
-          },
-        },
-        session?.jwt
-      )
-        .then(() => setLoader(true))
-        .then(() => setFiles([...files, audio])); */
-      /* setFiles([...files, audio]); //потом удалить */
     }
   };
 
   const handleAddSongPlaylist = (index) => {
     setPlaylist([...playlist, files[index]]);
-    console.log('Добавил песню: ', files[index]);
   };
   const handleDeleteSongPlaylist = (index) => {
     setPlaylist([...playlist.slice(0, index), ...playlist.slice(index + 1)]);
-    console.log('Удалил песню: ', files[index]);
   };
-
-  console.log('Твой плейлист', playlist);
 
   const uploadPlaylist = () => {
     setLoader(false);
@@ -112,10 +75,10 @@ export default function UserCollection() {
       .then(() => setLoader(true))
       .then(() => setPlaylistName(''))
       .then(() => setPlaylist([]));
-    console.log('Загруженный плейлист: ', playlist);
   };
 
   const upload = (index) => {
+    setLoader(false);
     createAudios(
       {
         data: {
@@ -128,7 +91,21 @@ export default function UserCollection() {
         },
       },
       session?.jwt
-    ).then(() => setLoader(true));
+    )
+      .then(() => setLoader(true))
+      .then(() =>
+        setList([
+          ...list,
+          {
+            id: Date.now(),
+            type: 'success',
+            description: 'Успешно загружено',
+          },
+        ])
+      )
+      .catch((error) => {
+        throw error;
+      });
   };
 
   const handlePlaylistName = (e) => {
@@ -137,6 +114,7 @@ export default function UserCollection() {
 
   return (
     <Layout>
+      <Toast toastlist={list} setList={setList} />
       <div>
         <DragAndDrop
           upload={upload}
